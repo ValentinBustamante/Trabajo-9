4766175fb0f2585a0d85c38d73e723aff13805aa
ValentinBustamante <140023829+ValentinBustamante@users.noreply.github.com>
2023-08-23 21:05:38 -0300

    Trabajo-9

---
+++ script.js
@@ -0,0 +1,19 @@
+let nombre ="valentin"; 
+let apellido = "bustamante";
+console.log(nombre+" "+apellido);
+
+let numerouno = 300
+let numerodos = 50
+let suma1 = numerouno + numerodos;
+console.log ("suma",suma1);
+let numerotres = 145
+let suma2 = numerouno + numerodos + numerotres;
+console.log ("suma",suma2);
+let numerocuatro = 155
+let suma3 = numerouno + numerodos + numerotres + numerocuatro;
+console.log ("suma", suma3);
+let numerocinco = 350
+let suma4 = numerouno + numerodos + numerotres + numerocuatro + numerocinco;
+console.log ("suma", suma4);
+
+ 
